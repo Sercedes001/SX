8424658b78d6c47b40f72818b09be2235394c43f
Sercedes / Cmd <121960555+Sercedes001@users.noreply.github.com>
2023-01-18 15:27:40 +0300

    Add files via upload

---
+++ ayarlar.js
@@ -0,0 +1,51 @@
+module.exports = {
+
+    token: "MTA2NDg2MjQ1MzY5Mjg5NTM0Mg.GCiupN.DFLlaL3zbyxYGtlAxmIGuRIuKlN9esGj_prSKo",
+    prefix: ".",
+    sahip: "927629673993764924",
+    durum: "Aktif",
+
+    api: {
+        TCKN: "",
+        TC_GSM: "C:\Users\Administrator\Desktop\discord-sorgu-main\zirvedebiraktik",
+        GSM_TC: "C:\Users\Administrator\Desktop\discord-sorgu-main\zirvedebiraktik",
+        AD_SOYAD: "C:\Users\Administrator\Desktop\discord-sorgu-main\zirvedebiraktik",
+        AILEA: "",
+        AILEB: "",
+        DDOS: "",
+        SECMEN_TCKN: "",
+        SECMEN_AD_SOYAD: "",
+        AOL: "",
+        ASI: "",
+        AD: "",
+        SOYAD: "",
+        IKAMETGAH: ""
+    },
+
+    emojiler: {
+        UPLOAD: ""
+    },
+
+    roller: {
+        admin: "1063170741211775057",
+        vip: "1063170742310686730",
+        premium: "1063170738951032933",
+        booster: "",
+        limitlirol: "",
+        freemium: "1063170673830281316",
+        yardimci: ""
+    },
+
+    log: "1065163740384870401",
+    log2: "1064593569228279959",
+
+    wl: [""], 
+
+    sunucuID: "1063153154239254620",
+    kanal: ["1065163861772214294"],
+    seskanal: "1063153154239254624",
+
+    sorgulimit: "3",
+
+    bakim: "YOK"
+}
